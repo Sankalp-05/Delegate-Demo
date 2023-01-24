3bb8f34dda5d7508e2e43cd71ecadf965e1bd952
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-24 17:13:14 +0530

    [Sankalp]- SingleCastDelegate and MultiCastDelegate

--- DelegateDemo/Program.cs
+++ DelegateDemo/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Delegate Demo Program ");
+            //operation obj = new operation(Demo.Addition);
+            //Console.WriteLine("Addition is = {0}", obj(23, 27));
+            MulticastDelegate.ImplementDelegate();
+            Console.ReadKey();
         }
     }
 }
